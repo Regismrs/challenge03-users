48079178ab6b031ced6b34f6be76655c44cbb67c
regismrs <regismrs@hotmail.com>
2023-11-06 23:47:15 -0300

    add: user repository

--- ms-user/src/main/java/com/compassuol/sp/challenge/msuser/repositories/UserRepository.java
+++ ms-user/src/main/java/com/compassuol/sp/challenge/msuser/repositories/UserRepository.java
@@ -1,2 +1,7 @@
-package com.compassuol.sp.challenge.msuser.repositories;public interface UserRepository {
+package com.compassuol.sp.challenge.msuser.repositories;
+
+import com.compassuol.sp.challenge.msuser.domain.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
 }
