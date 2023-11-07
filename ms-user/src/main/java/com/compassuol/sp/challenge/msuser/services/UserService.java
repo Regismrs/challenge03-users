3cd05c2f16c7b5fee9bfe6bb5b96a393bb770244
regismrs <regismrs@hotmail.com>
2023-11-07 02:03:16 -0300

    feat: integrate pubservice on create, update methods

--- ms-user/src/main/java/com/compassuol/sp/challenge/msuser/services/UserService.java
+++ ms-user/src/main/java/com/compassuol/sp/challenge/msuser/services/UserService.java
@@ -1,5 +1,6 @@
 package com.compassuol.sp.challenge.msuser.services;
 
+import com.compassuol.sp.challenge.msuser.domain.dto.Notification;
 import com.compassuol.sp.challenge.msuser.domain.dto.UserRequest;
 import com.compassuol.sp.challenge.msuser.domain.dto.UserResponse;
 import com.compassuol.sp.challenge.msuser.domain.entities.User;
@@ -8,21 +9,27 @@ import com.compassuol.sp.challenge.msuser.mapper.UserMapper;
 import com.compassuol.sp.challenge.msuser.repositories.UserRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
 public class UserService {
 
     private final UserRepository repository;
+    private final PublisherService pub;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, PublisherService publisherService) {
         repository = userRepository;
+        pub = publisherService;
     }
 
     public UserResponse createUser(UserRequest userRequest) {
-        return UserMapper.toDto(
-                repository.save(
-                        UserMapper.toUser(userRequest)));
+
+        User saved = repository.save(UserMapper.toUser(userRequest));
+
+        pub.send(userRequest.getEmail(), "CREATE");
+
+        return UserMapper.toDto(saved);
     }
 
     public UserResponse getUserById(Long id) throws NotFound {
@@ -33,6 +40,11 @@ public class UserService {
     public UserResponse updateUser(Long id, UserRequest userRequest) throws NotFound {
         User user = repository.findById(id)
                 .orElseThrow(() -> new NotFound("User not found"));
+
+        pub.send(String.format("OLD: %s, NEW: %s",
+                        user.getEmail(),
+                        userRequest.getEmail()),
+                "UPDATE");
 
         user.setFirstName(userRequest.getFirstName());
         user.setLastName(userRequest.getLastName());
