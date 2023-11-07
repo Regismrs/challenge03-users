88755097dbc88d247713ec9628bd527556905e50
regismrs <regismrs@hotmail.com>
2023-11-06 23:48:52 -0300

    feat: create, update, getbyid

--- ms-user/src/main/java/com/compassuol/sp/challenge/msuser/controllers/UserController.java
+++ ms-user/src/main/java/com/compassuol/sp/challenge/msuser/controllers/UserController.java
@@ -1,2 +1,45 @@
-package com.compassuol.sp.challenge.msuser.controllers;public class UserController {
+package com.compassuol.sp.challenge.msuser.controllers;
+
+import com.compassuol.sp.challenge.msuser.domain.dto.UserRequest;
+import com.compassuol.sp.challenge.msuser.domain.dto.UserResponse;
+import com.compassuol.sp.challenge.msuser.domain.entities.User;
+import com.compassuol.sp.challenge.msuser.services.UserService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<UserResponse> createUser(@RequestBody @Valid UserRequest userRequest) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(userService.createUser(userRequest));
+    }
+
+    @GetMapping("/users/{id}")
+    public ResponseEntity<UserResponse> getUserById(@PathVariable Long id) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(userService.getUserById(id));
+    }
+
+    @PutMapping("/users/{id}")
+    public ResponseEntity<UserResponse> updateUser(
+            @PathVariable Long id,
+            @RequestBody @Valid UserRequest userRequest) {
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(userService.updateUser(id, userRequest));
+    }
+
 }
--- ms-user/src/main/java/com/compassuol/sp/challenge/msuser/services/UserService.java
+++ ms-user/src/main/java/com/compassuol/sp/challenge/msuser/services/UserService.java
@@ -1,2 +1,47 @@
-package com.compassuol.sp.challenge.msuser.services;public class UserService {
+package com.compassuol.sp.challenge.msuser.services;
+
+import com.compassuol.sp.challenge.msuser.domain.dto.UserRequest;
+import com.compassuol.sp.challenge.msuser.domain.dto.UserResponse;
+import com.compassuol.sp.challenge.msuser.domain.entities.User;
+import com.compassuol.sp.challenge.msuser.exceptions.NotFound;
+import com.compassuol.sp.challenge.msuser.mapper.UserMapper;
+import com.compassuol.sp.challenge.msuser.repositories.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    private final UserRepository repository;
+
+    public UserService(UserRepository userRepository) {
+        repository = userRepository;
+    }
+
+    public UserResponse createUser(UserRequest userRequest) {
+        return UserMapper.toDto(
+                repository.save(
+                        UserMapper.toUser(userRequest)));
+    }
+
+    public UserResponse getUserById(Long id) throws NotFound {
+        return UserMapper.toDto(repository.findById(id)
+                .orElseThrow(() -> new NotFound("User not found")));
+    }
+
+    public UserResponse updateUser(Long id, UserRequest userRequest) throws NotFound {
+        User user = repository.findById(id)
+                .orElseThrow(() -> new NotFound("User not found"));
+
+        user.setFirstName(userRequest.getFirstName());
+        user.setLastName(userRequest.getLastName());
+        user.setCpf(userRequest.getCpf());
+        user.setEmail(userRequest.getEmail());
+        user.setActive(userRequest.getActive());
+        user.setBirthdate(userRequest.getBirthdate());
+
+        return UserMapper.toDto(repository.save(user));
+    }
+
 }
